b64b1239f7cecbad7596e7781a91d41f5b0c9965
RCaria <caria.rui@hotmail.com>
2022-06-11 19:01:40 +0100

    first DOCOPT draft

--- src/client.py
+++ src/client.py
@@ -1,12 +1,37 @@
-"""
+'''
 TFTPy - This module implements an interactive and command line TFTP 
 client.
 
-This client accepts the following options:
-    $ python3 client.py (get|put) [-p serv_port] server source_file [dest_file] 
-    $ python3 client.py [-p serv_port] server
+Usage: 
+  client.py [get|put] [-p serv_port] <server> [<source_file> [<dest_file>]]
 
-(C) João Galamba, 2022
-"""
+Options: 
+-h --help       show help
+get             get file from server
+put             send file to server
+-p serv_port    [default: 69] specify a communication port
+server          server IP or name
+source_file     name of the source file
+dest_file       name for the destination file
+
+
+'''
+
+from click import UsageError
+import docopt
+import sys
+
+args=docopt.docopt(__doc__)
+print(args)
+
+
+# specified por is not a number -> exit 
+if not args["-p"].isnumeric():
+    raise docopt.DocoptExit
+else:
+    if not args.get("<source_file>"): # no source file specified -> interactive
+        comm = input("tftp client>")    
+    if not args.get("<dest_file>"): # no destination file specified -> use source filename
+        args["<dest_file = args>"]=args["<source_file>"]
 
 
